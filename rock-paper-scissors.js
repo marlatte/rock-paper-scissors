66d7282ae208bf726d533879e984a1cb9ecb5e1b
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-05-08 20:58:48 -0400

    Add basic buttons and some CSS

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -78,7 +78,10 @@ game()
 */
 
 
-// INGREDIENTS
+const buttons = document.querySelectorAll("button");
+let scoreboard = document.querySelector(".scoreboard");
+
+
 function generateRandomInts(lowerLimit, upperLimit, amount = 1) {
     for (let index = 0; index < amount; index++) {
         const result = Math.floor(Math.random() * (upperLimit - lowerLimit)) + lowerLimit;
@@ -92,51 +95,9 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-function getPlayerChoice(roundNumber) {
-    let playerSelection = "";
-    let promptAgain = confirm(`Ready for Round ${roundNumber}?`);
-    while (promptAgain) {
-        let playerInput = prompt("What do you choose?", "");
-        if (playerInput === null) {
-            playerInput = "exit";
-        }
-        switch (playerInput.toLowerCase()) {
-            case "exit":
-                promptAgain = false;
-                break;
-
-            case "":
-                alert("You have to make a choice! \nTry Again");
-                promptAgain = true
-                break;
-        
-            case "rock":
-                playerSelection = "Rock";
-                promptAgain = false;
-                break;
-    
-            case "paper":
-                playerSelection = "Paper";
-                promptAgain = false;
-                break;
-
-            case "scissors":
-                playerSelection = "Scissors";
-                promptAgain = false;
-                break;
-    
-            default:
-                alert("Not a valid choice. \nTry Again")
-                promptAgain = true;
-                break;
-        }
-    }
-    return playerSelection;
-}
-
-function printSelections(playerSelection, computerSelection, roundNumber) {
-    console.log(`(${roundNumber}) Player selects: ${playerSelection}`);
-    console.log(`(${roundNumber}) Computer selects: ${computerSelection}`);
+function printSelections(playerSelection, computerSelection) {
+	document.querySelector(".player-selects").textContent = `Player selects: ${playerSelection}`;
+    document.querySelector(".computer-selects").textContent = `Computer selects: ${computerSelection}`;
 }
 
 function checkWin(playerSelection, computerSelection) {
@@ -173,28 +134,22 @@ function checkOutcome(playerWin, playerSelection, computerSelection) {
     return [roundOutcome, winningHand, losingHand]
 }
 
-function playRound(roundNumber) {
-    let playerSelection = getPlayerChoice(roundNumber);
+function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
     let tieGame = (playerSelection === computerSelection);
-    if (playerSelection === "") {
-        console.log("Round cancelled.")
-        roundOutcome = "cancelled"
-        return roundOutcome;
-    }
-    else if (tieGame) {
-        printSelections(playerSelection, computerSelection,roundNumber);
+	if (tieGame) {
+        printSelections(playerSelection, computerSelection);
         roundOutcome = "tie"
-        console.log(`(${roundNumber}) It's a tie.`)
+		document.querySelector(".round-outcome").textContent = `It's a tie.`;
         return roundOutcome;
     } else {
-        printSelections(playerSelection, computerSelection, roundNumber);
+        printSelections(playerSelection, computerSelection);
         let playerWin = checkWin(playerSelection,  computerSelection);
         let resultArray = checkOutcome(playerWin, playerSelection, computerSelection);
         let roundOutcome = resultArray[0];
         let winningHand = resultArray[1];
         let losingHand  = resultArray[2];
-        console.log(`(${roundNumber}) You ${roundOutcome}! ${winningHand} beats ${losingHand}.`);
+        document.querySelector(".round-outcome").textContent = `You ${roundOutcome}! ${winningHand} beats ${losingHand}.`;
         return roundOutcome;
     };
 }
@@ -215,50 +170,71 @@ function printScores(playerScore, computerScore, roundNumber) {
     console.log(`(${roundNumber}) Computer Score: ${computerScore}`)
  }
 
- function game(gameLength = 5) {
-    let playerScore = 0;
-    let computerScore = 0;
-    let mercyRule = gameLength/2;
-    let roundNumber;
-    let roundOutcome;
-    gameLength = prompt(`Let's play! Best of ${gameLength}?`, gameLength)
-    if (gameLength ===  null) {
-        roundOutcome = "cancelled"
-    } else {
-        for (let i = 0; i < gameLength; i++) {
-            roundNumber = i + 1;
-            roundOutcome  = playRound(roundNumber);
-            if (roundOutcome === "cancelled") {
-                console.log(`Game cancelled in round ${roundNumber}`)
-                i = gameLength;
-            } else {
-                let roundWinner = whoWon(roundOutcome)      
-                alert(`(${roundNumber}) Round ${roundNumber} goes to ${roundWinner}!`)
-                console.log(`(${roundNumber}) Round ${roundNumber} goes to ${roundWinner}!`)
-                if (roundWinner === "Player") {
-                    playerScore++;
-                } else if (roundWinner === "Computer") {
-                    computerScore++;
-                }
-                printScores(playerScore, computerScore, roundNumber);
-                if (playerScore > mercyRule || computerScore >mercyRule)  {
-                    gameLength = i; 
-                    /*Should there be a way to invoke the mercy rule if the losing side can't possibly catch up? 
-                    Eg. if player is losing 2-4 in round 8 of a 9-round game, there's no point in playing round 9. */
+//  function game(gameLength = 5) {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let mercyRule = gameLength/2;
+//     let roundNumber;
+//     let roundOutcome;
+//     gameLength = prompt(`Let's play! Best of ${gameLength}?`, gameLength)
+//     if (gameLength ===  null) {
+//         roundOutcome = "cancelled"
+//     } else {
+//         for (let i = 0; i < gameLength; i++) {
+//             roundNumber = i + 1;
+//             roundOutcome  = playRound(roundNumber);
+//             if (roundOutcome === "cancelled") {
+//                 console.log(`Game cancelled in round ${roundNumber}`)
+//                 i = gameLength;
+//             } else {
+//                 let roundWinner = whoWon(roundOutcome)      
+//                 alert(`(${roundNumber}) Round ${roundNumber} goes to ${roundWinner}!`)
+//                 console.log(`(${roundNumber}) Round ${roundNumber} goes to ${roundWinner}!`)
+//                 if (roundWinner === "Player") {
+//                     playerScore++;
+//                 } else if (roundWinner === "Computer") {
+//                     computerScore++;
+//                 }
+//                 printScores(playerScore, computerScore, roundNumber);
+//                 if (playerScore > mercyRule || computerScore >mercyRule)  {
+//                     gameLength = i; 
+//                     /*Should there be a way to invoke the mercy rule if the losing side can't possibly catch up? 
+//                     Eg. if player is losing 2-4 in round 8 of a 9-round game, there's no point in playing round 9. */
                     
-                    // NO! That's too much requirements creep.
-                } else if (i === (gameLength - 1) && playerScore  === computerScore) {
-                    alert("Tie breaker!");
-                    gameLength++;
-                }
-            }
-        }
-    }
-    if (roundOutcome != "cancelled" && playerScore > computerScore) {
-        console.log(`You win! ${playerScore} - ${computerScore} in ${roundNumber} rounds.`)
-    } else if (roundOutcome != "cancelled" && playerScore < computerScore) {
-        console.log(`You lose! ${playerScore} - ${computerScore} in ${roundNumber} rounds.`)
-    } else {
-        return `Game ${roundOutcome}.`
-    }
-}
+//                     // NO! That's too much requirements creep.
+//                 } else if (i === (gameLength - 1) && playerScore  === computerScore) {
+//                     alert("Tie breaker!");
+//                     gameLength++;
+//                 }
+//             }
+//         }
+//     }
+//     if (roundOutcome != "cancelled" && playerScore > computerScore) {
+//         console.log(`You win! ${playerScore} - ${computerScore} in ${roundNumber} rounds.`)
+//     } else if (roundOutcome != "cancelled" && playerScore < computerScore) {
+//         console.log(`You lose! ${playerScore} - ${computerScore} in ${roundNumber} rounds.`)
+//     } else {
+//         return `Game ${roundOutcome}.`
+//     }
+// }
+
+
+buttons.forEach(button => {
+	button.addEventListener("click", (e) => {
+		let playerSelection = e.target.id;
+		switch (playerSelection) {
+			case "rock":
+                playerSelection = "Rock";
+                break;
+    
+            case "paper":
+                playerSelection = "Paper";
+                break;
+
+            case "scissors":
+                playerSelection = "Scissors";
+                break;
+		}
+		playRound(playerSelection);
+	})
+});
