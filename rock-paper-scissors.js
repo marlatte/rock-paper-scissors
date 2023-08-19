4071ca31a66fa0b1cb720b0479a9808cf9977370
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-08-18 23:41:08 -0400

    Translate psuedo to code and debug

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -30,34 +30,108 @@ const startGameBtn = document.getElementById("start-game-btn");
 
 
 // -- Behind the Scenes -- //
+function handleUserChoice(e) {
+	let userChoice = (e.target.classList.contains("button") ? e.target.id : e.target.parentElement.id).slice(0, -4);
+	showUserChoice(userChoice);
+	playRound(userChoice);
+}
 
+function playRound(userChoice) {
+	let computerChoice = ["rock", "paper", "scissors"][Math.floor(Math.random() * (3))];
+	displayComputerChoice(computerChoice);
+	let outcome = userChoice === computerChoice ? "tie" : getWinner(userChoice, computerChoice);
+	displayOutcome(outcome);
+	updateScores(outcome);
+	if (userScore >= 5 || computerScore >= 5) {
+		gameButtons.forEach((button) => button.disabled = true)
+		setTimeout(() => endGame(userScore > computerScore), 400);
+	}
+}
 
+function getWinner(userChoice, computerChoice) {
+	return {
+		"rock": (computerChoice === "scissors"),
+		"paper": (computerChoice === "rock"),
+		"scissors": (computerChoice === "paper"),
+	}[userChoice] ? "victory" : "defeat";
+}
 
 // -- On-Screen Content -- //
 function startNewGame() {
-	roundCounter = 1;
-	userScore = 0;
-	computerScore = 0;
+	gameButtons.forEach((button) => button.disabled = true);
+	updateScores("reset");
 	computerChoiceDisplay.classList = "shrunk";
 	modal.classList.toggle("shrunk");
-	displayMessage(3, 2, 1, "fight!");
-	setTimeout(() => message.classList.add("shrunk"), 4300);
-	welcomeMsg.classList.toggle("hidden");
+	showCountdown(3, 2, 1, "fight!");
+	setTimeout(() => {
+		message.classList.add("shrunk");
+		gameButtons.forEach((button) => button.disabled = false);
+	}, 3000);
+	setTimeout(() => welcomeMsg.classList.toggle("hidden"), 1000);
 }
 
-function displayMessage(...msgArray) {
+function showCountdown(...msgArray) {
 	let i = 0;
 	const countdown = setInterval(() => {
-		// debugger
 		if (i === msgArray.length) {
 			clearInterval(countdown);
 		} else {
 			message.textContent = msgArray[i];
 			i++;
 		}
-	}, 1000);
+	}, 700);
 }
 
+function showUserChoice(userChoice) {
+	let userChoiceBtn = document.getElementById(`${userChoice}-btn`);
+	userChoiceBtn.classList.add("selected");
+	setTimeout(() => userChoiceBtn.classList.remove("selected"), 500);
+}
+
+function displayComputerChoice(computerChoice) {
+	computerChoiceDisplay.classList = `fa-regular fa-hand${{
+		"rock": "-back-fist",
+		"paper": "",
+		"scissors": "-scissors",
+	}[computerChoice]}`
+}
+
+function displayOutcome(outcome) {
+	message.textContent = outcome;
+	message.classList.remove("shrunk");
+}
+
+function updateScores(outcome) {
+	if (outcome === "victory") {
+		userProgress.value = ++userScore;
+		userProgress.textContent = userScore;
+		roundDisplay.firstElementChild.textContent = ++roundCounter;
+	} else if (outcome === "defeat") {
+		computerProgress.value = ++computerScore;
+		computerProgress.textContent = computerScore;
+		roundDisplay.firstElementChild.textContent = ++roundCounter;
+	} else if (outcome === "reset") {
+		userScore = 0;
+		userProgress.value = 0;
+		userProgress.textContent = 0;
+		computerScore = 0
+		computerProgress.value = 0;
+		computerProgress.textContent = 0;
+		roundCounter = 1;
+		roundDisplay.firstElementChild.textContent = 1;
+	}
+}
+
+function endGame(winnerBoolean) {
+	gameOverMsg.lastElementChild.textContent = winnerBoolean ? "You Win!" : "You Lose!";
+	gameOverMsg.style.color = winnerBoolean ? "var(--text-primary)" : "red";
+	startGameBtn.textContent = "play again";
+	startGameBtn.style.fontSize = "22px";
+	modal.classList.toggle("shrunk");
+	gameOverMsg.classList = "game-over headers";
+	message.textContent = "";
+	computerChoiceDisplay.classList = "shrunk";
+}
 
 // -------------------------------------------------------------------------------- //
 // --------------------     Calls & Event Listeners    ---------------------------- //
@@ -70,85 +144,10 @@ function displayMessage(...msgArray) {
 // -- On-Screen Content -- //
 welcomeMsg.classList = "welcome headers";
 
-startGameBtn.addEventListener("click", startNewGame);
+startGameBtn.addEventListener("click", () => {
+	startGameBtn.style.scale = 1.1;
+	setTimeout(() => startGameBtn.style.scale = 1, 100);
+	setTimeout(startNewGame, 130);
+});
 
-
-/*
-PSEUDO
-
-FUNCTION displayMessage(msgArray, interval)
-	FOR (let el of msgArray)
-		message.textContent = el;
-		message.classList.toggle("shrunk");
-		setTimeout(() => message.classList.toggle("shrunk"), (interval * 1000));
-	END FOR
-END FUNCTION
-
-
-FOR button OF gameButtons
-	EVENT LISTENER button MOUSE ENTER
-		button.classList.add("hover");
-	END EVENT LISTENER
-
-	EVENT LISTENER button MOUSE LEAVE
-		button.classList.remove(.hover);
-	END EVENT LISTENER
-
-	EVENT LISTENER button ON CLICK
-		handleUserChoice(e)
-	END EVENT LISTENER
-END FOR
-
-FUNCTION handleUserChoice(e)
-	let userChoice = e.target.classList.contains("button") ? e.target.id : e.target.parentElement.id;
-	showPlayerChoice(userChoice);
-	playRound(userChoice);
-END FUNCTION
-
-FUNCTION showPlayerChoice(userChoice)
-	let playerChoiceBtn = document.getElementByID(`${userChoice}`);
-	playerChoiceBtn.classList.add("selected");
-	setTimeout(() => playerChoiceBtn.classList.remove("selected"), 3000);
-END FUNCTION
-
-FUNCTION playRound(userChoice)
-	let computerChoice = ["rock", "paper", "scissors"][Math.floor(Math.random() * (3))];
-	displayComputerChoice(computerChoice);
-	let outcome = userChoice === computerChoice ? "tie" : getWinner(userChoice, computerChoice);
-	displayMessage([outcome], #);
-	IF (outcome === "victory")
-		userProgress.value = ++userScore;
-	IF (outcome === "defeat")
-		computerProgress.value = ++computerScore;
-	END IF
-	roundDisplay.firstElementChild.textContent = ++roundCounter;
-	IF userScore >= 5 || computerScore >= 5
-		endGame(userScore > computerScore)
-END FUNCTION
-
-FUNCTION displayComputerChoice(computerChoice)
-	const computerDictionary = {
-		"rock" : "-back-fist", 
-		"paper" : "",
-		"scissors" : "-scissors",
-	}
-	computerChoiceDisplay.classList = `fa-regular fa-hand${computerDictionary[computerChoice]}`
-END FUNCTION
-
-FUNCTION getWinner(userChoice, computerChoice)
-	return {
-		"rock-btn" : (computerChoice === "scissors"),
-		"paper-btn" : (computerChoice === "rock"),
-		"scissors-btn" : (computerChoice === "paper"),
-	}[userChoice] ? "victory" : "defeat";
-END FUNCTION
-
-FUNCTION endGame(winnerBoolean)
-	gameOverMsg.lastElementChild.textContent = winnerBoolean ? "You Win!" : "You Lose!";
-	gameOverMsg.style.color = winnerBoolean ? "var(--text-primary)" : "red";
-	startGameBtn.textContent = "play again";
-	startGameBtn.style.fontSize = "22px";
-	modal.classList.toggle("shrunk");
-	gameOverMsg.classList = "game-over headers";
-END FUNCTION
-*/
+gameButtons.forEach((button) => button.addEventListener("click", handleUserChoice)) 
